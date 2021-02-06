27acee5a4380d3d0f54ac68af113ef9bfa6acf31
Tyler Grissom <minestein123@gmail.com>
2021-02-06 14:30:52 -0600

    Light mode/dark mode, updated design

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,36 @@
+const body = $('body')
+const footer = $('footer')
+
 const ipField = $('#mc-ip')
 const ipCopyText = $('#mc-ip-copy-text')
 const oldText = ipCopyText.html()
 
+const buttons = $('.btn')
+
 $(() => {
     ipCopyText.hide()
 
-    ipField.on('mouseover', () => ipCopyText.show()).on('mouseout', () => ipCopyText.hide())
+    loadTheme()
 
-    ipField.on('click',() => {
-        clip()
+    const btnDark = $('#btn-dark')
+    const btnLight = $('#btn-light')
+
+    btnDark.on('click', () => {
+        body.addClass('light-mode')
+        loadTheme()
     })
+
+    btnLight.on('click', () => {
+        body.removeClass('light-mode')
+        loadTheme()
+    })
+
+    ipCopyText.hide()
+
+    ipField
+        .on('mouseover', () => ipCopyText.show())
+        .on('mouseout', () => ipCopyText.hide())
+        .on('click', () => clip())
 })
 
 function clip() {
@@ -20,4 +41,18 @@ function clip() {
     setTimeout(function () {
         ipCopyText.text(oldText).removeClass('accent-text')
     }, 4000)
+}
+
+function loadTheme() {
+    if (body.hasClass('light-mode')) {
+        $('#btn-dark').hide()
+        $('#btn-light').show()
+        buttons.removeClass('btn-outline-light').addClass('btn-outline-dark')
+        footer.addClass('light-mode')
+    } else {
+        $('#btn-light').hide()
+        $('#btn-dark').show()
+        buttons.removeClass('btn-outline-dark').addClass('btn-outline-light')
+        footer.removeClass('light-mode')
+    }
 }
